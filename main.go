0700dbc7f6d88075ee55d2263330e543946f0632
Miriah Peterson <mpeterson@nav.com>
2018-10-13 14:01:06 -0600

    add go routine

--- main.go
+++ main.go
@@ -4,46 +4,59 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"golang.org/x/net/html"
 )
 
 func main() {
-	//	client := http.DefaultClient
 	client := &http.Client{
 		Timeout: time.Second * 5,
 	}
+	in := make(chan string, 10)
+	go request(client, in)
+	in <- "https://air.utah.gov"
 
-	resp, err := get(client, "https://air.utah.gov")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	//	if len(doc.Nodes) == 0 {
-	//		panic("no nodes found")
+	//	resp, err := get(client, "https://air.utah.gov")
+	//	if err != nil {
+	//		panic(err)
 	//	}
-	if err != nil {
-		panic(err)
-	}
-	selection := doc.Find("a")
-	if len(selection.Nodes) == 0 {
-		panic("no 'a' nodes found")
-	}
-	for _, node := range selection.Nodes {
-		for _, attr := range node.Attr {
-			if attr.Key == "href" {
-				if strings.HasPrefix(attr.Val, "http") {
-					err = getChild(client, attr)
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-				break
+	//	defer resp.Body.Close()
+	//	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	selection := doc.Find("a")
+	//	if len(selection.Nodes) == 0 {
+	//		panic("no 'a' nodes found")
+	//	}
+	//	for _, node := range selection.Nodes {
+	//		for _, attr := range node.Attr {
+	//			if attr.Key == "href" {
+	//				if strings.HasPrefix(attr.Val, "http") {
+	//					err = getChild(client, attr)
+	//					if err != nil {
+	//						fmt.Println(err)
+	//					}
+	//				}
+	//				break
+	//			}
+	//		}
+	//	}
+}
+
+func request(client *http.Client, in chan string) {
+	for {
+		select {
+		case url := <-in:
+			resp, err := get(client, url)
+			if err != nil {
+				fmt.Println(err)
+				continue
 			}
+			// add routines to parse the childs
+			// time go run main.go
+			fmt.Println(resp.StatusCode)
 		}
 	}
 }
@@ -68,7 +81,6 @@ func getChild(client *http.Client, attr html.Attribute) error {
 }
 
 func get(client *http.Client, url string) (*http.Response, error) {
-	// client.Get("https://air.utah.gov")
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -82,15 +94,8 @@ func get(client *http.Client, url string) (*http.Response, error) {
 	if err != nil {
 		return nil, err
 	}
-	//	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("not a 200 status code")
 	}
-	//	body, err := ioutil.ReadAll(resp.Body)
-	//	if err != nil {
-	//		return err
-	//	}
-	//	fmt.Println(string(body))
 	return resp, err
-	//defer is called here
 }
